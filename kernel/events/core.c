83fbfec5380542ae90004a78ca3c9fe16b342362
Psycho666 <justinvanschijndel@hotmail.com>
2016-01-21 22:06:04 +0100

    patch-3.10.55-56
    Signed-off-by: Psycho666 <justinvanschijndel@hotmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1420,6 +1420,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
@@ -1849,6 +1854,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
