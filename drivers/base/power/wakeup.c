aebff86dc2e95d97361175c0d080e9dd183c1aff
Psycho666 <justinvanschijndel@hotmail.com>
2016-03-05 21:35:31 +0100

    Revert "power: Add Toggle To Remove SensorHub Wakelock"
    This reverts commit e44d06b1e354f99d9f6d296a79741b89584109da.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -18,11 +18,6 @@
 
 #include "power.h"
 
-#include <linux/moduleparam.h>
-
-static bool enable_sensorhub_wl = true;
-module_param(enable_sensorhub_wl, bool, 0644);
-
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
@@ -386,11 +381,6 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
-	
-	if (!enable_sensorhub_wl && !strcmp(ws->name, "ssp_sensorhub_wake_lock")) {
-		pr_info("wakeup source sensorhub activation skipped\n");
-		return;
-	}
 
 	/*
 	 * active wakeup source should bring the system
